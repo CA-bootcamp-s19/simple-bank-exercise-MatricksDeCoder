3c1125f0440ab66eed6be836c22445b6c06dca0a
Zvinodashe Mupambirei <MatricksDeCoder@users.noreply.github.com>
2020-11-09 16:12:43 +0200

    Update SimpleBank.sol
    added left out transferring funds to withdrawer

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -45,15 +45,6 @@ contract SimpleBank {
         owner = msg.sender;
     }
 
-    // Fallback function - Called if other functions don't match call or
-    // sent ether without data
-    // Typically, called when invalid data is sent
-    // Added so ether sent to this contract is reverted if the contract fails
-    // otherwise, the sender's money is transferred to contract
-    fallback() external payable {
-        revert();
-    }
-
     /// @notice Get balance
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
@@ -95,9 +86,10 @@ contract SimpleBank {
     // Emit the appropriate event    
     function withdraw(uint withdrawAmount) public returns (uint) {
         
-        require(enrolled[msg.sender] ==true);
+        require(enrolled[msg.sender] == true);
         require(balances[msg.sender] >= withdrawAmount);
         balances[msg.sender] -= msg.value;
+        msg.sender.transfer(withdrawAmount);
         emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
         return balances[msg.sender];
         
@@ -105,6 +97,15 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
+    }
+    
+    // Fallback function - Called if other functions don't match call or
+    // sent ether without data
+    // Typically, called when invalid data is sent
+    // Added so ether sent to this contract is reverted if the contract fails
+    // otherwise, the sender's money is transferred to contract
+    fallback() external payable {
+        revert();
     }
 
 }
